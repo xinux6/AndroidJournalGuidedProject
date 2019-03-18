29ff236120833adc984eb08e117cc53e95444432
Chance Payne <chance@lambdaschool.com>
2019-03-18 12:20:10 -0600

    Initial commit

---
+++ app/src/main/java/com/lambdaschool/journalguidedproject/JournalEntry.java
@@ -0,0 +1,54 @@
+package com.lambdaschool.journalguidedproject;
+
+public class JournalEntry {
+    private String date, entryText, image;
+    private int dayRating, id;
+
+    public JournalEntry(String date, String entryText, String image, int dayRating, int id) {
+        this.date = date;
+        this.entryText = entryText;
+        this.image = image;
+        this.dayRating = dayRating;
+        this.id = id;
+    }
+
+    public JournalEntry(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEntryText() {
+        return entryText;
+    }
+
+    public void setEntryText(String entryText) {
+        this.entryText = entryText;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getDayRating() {
+        return dayRating;
+    }
+
+    public void setDayRating(int dayRating) {
+        this.dayRating = dayRating;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ app/src/main/java/com/lambdaschool/journalguidedproject/MainActivity.java
@@ -0,0 +1,117 @@
+package com.lambdaschool.journalguidedproject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int IMAGE_REQUEST_CODE = 50;
+
+    static int nextId = 0;
+
+    private JournalEntry entry;
+
+    private TextView  dateView;
+    private EditText  entryTextView;
+    private SeekBar   dayRatingView;
+    private ImageView dayImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createJournalEntry();
+
+        dateView = findViewById(R.id.journal_entry_date);
+        dateView.setText(entry.getDate());
+
+        entryTextView = findViewById(R.id.journal_entry_text);
+        entryTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String entryString = s.toString();
+                entry.setEntryText(entryString);
+            }
+        });
+
+        dayRatingView = findViewById(R.id.journal_entry_seekbar);
+        dayRatingView.setMax(5);
+        dayRatingView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser) {
+                    entry.setDayRating(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        dayImageView = findViewById(R.id.journal_entry_image);
+
+        findViewById(R.id.add_image_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
+            }
+        });
+    }
+
+    private void createJournalEntry() {
+        entry = new JournalEntry(nextId++);
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date       date       = new Date();
+
+        entry.setDate(dateFormat.format(date));
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK && requestCode == IMAGE_REQUEST_CODE) {
+            if (data != null) {
+                Uri dataUri = data.getData();
+                entry.setImage(dataUri.toString());
+
+                dayImageView.setImageURI(dataUri);
+            }
+        }
+    }
+}
